b9b35621426caec52798ba1d1f781023c675a87a
snailgclq <snail_gclq@163.com>
2019-08-28 20:34:50 +0800

    短信验证重构，代码结构整理

--- src/main/java/com/woniu/woniuticket/platform_user/PlatformUserApplication.java
+++ src/main/java/com/woniu/woniuticket/platform_user/PlatformUserApplication.java
@@ -4,11 +4,33 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @MapperScan(value = "com.woniu.woniuticket.platform_user.mapper")
 @EnableCaching
-public class PlatformUserApplication {
+public class PlatformUserApplication  implements TransactionManagementConfigurer {
+
+    @Resource(name = "txManager")
+    private PlatformTransactionManager txManager;
+
+    //创建事务管理
+    @Bean
+    public PlatformTransactionManager txManager(DataSource dataSource){
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    // 实现接口 TransactionManagementConfigurer 方法，其返回值代表在拥有多个事务管理器的情况下默认使用的事务管理器
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return txManager;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PlatformUserApplication.class, args);
--- src/main/java/com/woniu/woniuticket/platform_user/controller/UserController.java
+++ src/main/java/com/woniu/woniuticket/platform_user/controller/UserController.java
@@ -4,20 +4,15 @@ import com.alibaba.fastjson.JSON;
 import com.woniu.woniuticket.platform_user.constant.UserConstant;
 import com.woniu.woniuticket.platform_user.pojo.User;
 import com.woniu.woniuticket.platform_user.service.UserService;
-import com.woniu.woniuticket.platform_user.utils.UserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,12 +23,6 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private JavaMailSender mailSender;
-
-    @Autowired
-    private WalletController walletController;
-
     /**
      * 用户注册
      * @param user
@@ -43,95 +32,49 @@ public class UserController {
     @Transactional
     @PostMapping("/user")
     public String userRegisty(@RequestBody @Validated User user, BindingResult br){ //@Validated
-//        ModelAndView mv = new ModelAndView();
-        Map<String,Object> verify = new HashMap();
+        Map<String,Object> verify = new HashMap();      //存放返回消息
         System.out.println("**********进入注册方法**********");
         System.out.println("接收的参数："+user);
-        String result = fomatVerify(verify,br);
+        //格式验证（返回json_Map数据）
+        String result = userService.fomatVerify(verify,br);
         if(result!=null){
             return result;
         }
-        result = dataVerify(user,verify);
+        //数据验证（返回json_Map数据）
+        result = userService.dataVerify(user,verify);
         if(result!=null){
             return result;
         }
 
-        if(user.getRegistCode()!=null && user.getRegistCode()!=""){
-            System.out.println("有注册码");
-            //根据注册码查找父用户
-            User findUser = userService.findUserByRegistCode(user.getRegistCode());
-            if (findUser!=null){
-                Map error = walletController.createWallet(findUser.getUserId());
+        //验证完毕进行初始化
+        user = userService.userInit(user);
+
+        if(user.getRegistCode()!=null && user.getRegistCode()!="" ){
+            /*有注册码的处理*/
+            if(user.getRegistCode().length()==32){
+                System.out.println("有注册码");
+                //根据注册码查找父用户
+                User findUserFather = userService.findUserByRegistCode(user.getRegistCode());
+                if(findUserFather==null){
+                    verify.put("registryCodeError","无效的注册码");
+                    return JSON.toJSONString(verify);
+                }
+                userService.registryCode(user,findUserFather.getUserId());
+                userService.sendEmail(user,verify);
+                verify.put("message","注册并发送激活邮件成功");
+            }else{
+                verify.put("registryCodeError","无效的注册码");
+                return JSON.toJSONString(verify);
             }
-
-
-            //修改策略，使用事务控制插入用户与同生钱包
-
-
-
-
-
-//            if(result!=null){
-//                return result;
-//            }
-//            result = dataVerify(user,verify);
-//            if(result!=null){
-//                return result;
-//            }
-
-            /*
-                这里处理有注册码的情况，
-                比如优惠券
-
-                根据 ·注册码· 查找拥有该 ·邀请码· 用户，
-                携带注册码用户 + 券
-                携带邀请码用户 + 券
-
-                1.根据注册码查拥有该邀请码用户（验证注册码正确性）
-                2.根据查找到的用户id查钱包 + 券（修改券操作）
-                3.注册用户 + 券（修改券操作）
-                4.注册用户执行insert后才会有钱包，（注册完毕同时insert钱包）
-                5.注册用户执行insert之后才能根据id查找钱包或者通过id修改钱包
-                so，这段代码需要放在插入语句之后
-
-             */
-//        }else{
-//            if(result!=null){
-//                return result;
-//            }
-//            result = dataVerify(user,verify);
-//            if(result!=null){
-//                return result;
-//            }
-        }
-
-
-
-        //对传入密码进行md5加密
-        String md5Password = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
-        user.setPassword(md5Password);
-        //设置用户32位邀请码
-        user.setInviteCode(UserUtil.getRandomString(32));
-        user.setRegistTime(new Date());
-        user.setVipState(0);
-        user.setUserState(0);
-        System.out.println("\n最终插入数据："+user);
-
-        //用户记录插入
-        int message = userService.insert(user);
-        if (message==1){
-            verify.put("message","注册成功,邮件已发送，请入邮箱激活。");
-//            return null;
-        }else {
-            verify.put("message","注册失败，发生未知错误");
-        }
-        //激活邮件发送
-        try {
-            UserUtil.sendEmail(mailSender,user);
-        } catch (Exception e) {
-            e.printStackTrace();
-            verify.put("message","注册失败，发生未知错误\n"+e);
-            return JSON.toJSONString(verify);
+        }else{
+            /*无注册码的处理*/
+            System.out.println("无注册码");
+            userService.registryNoCode(user);
+            String email = userService.sendEmail(user,verify);
+            if(email!=null){
+                return email;
+            }
+            verify.put("message","注册并发送激活邮件成功");
         }
         System.out.println("**********退出注册方法**********");
         return JSON.toJSONString(verify);
@@ -143,26 +86,23 @@ public class UserController {
      * @return
      */
     @GetMapping("/user/email/{userId}")
-    public ModelAndView emailActivate(@PathVariable Integer userId,HttpServletRequest req){
+    public String emailActivate(@PathVariable Integer userId,HttpServletRequest req){
         System.out.println("**********进入邮件激活方法**********");
-        ModelAndView mv = new ModelAndView();
         System.out.println(userId);
         User user = userService.selectByPrimaryKey(userId);
         System.out.println("根据id查找到的用户："+user);
         if(user.getUserState()==0) {
             int message = userService.updateStateByKey(userId);
             if (message == 1) {
-                mv.addObject("message", "该账户激活成功！");
                 req.getSession().setAttribute(UserConstant.USER_LOGIN,user);
             } else {
-                mv.addObject("message", "激活失败");
+                return "激活失败";
             }
         }else{
-            mv.addObject("message", "该用户已经激活，请勿多次激活");
+            return "该用户已经激活，请勿多次激活";
         }
         System.out.println("**********退出邮件激活方法**********");
-        mv.setViewName(UserConstant.MESSAGE);
-        return mv;
+        return "邮件激活成功...";
     }
 
 
@@ -219,70 +159,9 @@ public class UserController {
         return null;
     }
 
-    /**
-     * 用户注册格式验证（普通注册）
-     * @param verify    验证结果容器
-     * @param br        格式验证
-     * @return
-     */
-    public String fomatVerify(Map verify,BindingResult br){
-        //获取错误信息的总数
-        int errorCount = br.getErrorCount();
-        //格式验证
-        if(errorCount>0) {
-            //获取错误消息字段
-            FieldError nameError = br.getFieldError("userName");
-            FieldError pwdError = br.getFieldError("password");
-            FieldError emailError = br.getFieldError("email");
-            FieldError mobileError = br.getFieldError("mobile");
-            FieldError nicknameError = br.getFieldError("nickname");
-            if (nameError != null) {
-                //将验证的错误消息存进ModelAndView
-                verify.put("nameError", nameError.getDefaultMessage());
-            }
-            if (pwdError != null) {
-                verify.put("pwdError", pwdError.getDefaultMessage());
-            }
-            if (emailError != null) {
-                verify.put("emailError", emailError.getDefaultMessage());
-            }
-            if (mobileError != null) {
-                verify.put("mobileError", mobileError.getDefaultMessage());
-            }
-            if (nicknameError != null) {
-                verify.put("nicknameError", nicknameError.getDefaultMessage());
-            }
-            return JSON.toJSONString(verify);
-        }
-        return null;
-    }
 
-    /**
-     * 用户注册数据验证（普通注册）
-     * @param user      注册信息
-     * @param verify    验证结果容器
-     * @return
-     */
-    public String dataVerify(User user,Map verify){
-        User findUserByName = userService.findUserByName(user.getUserName());
-        User findUserByEmail = userService.findUserByEmail(user.getEmail());
-        User findUserByMobile = userService.findUserByMobile(user.getMobile());
-        //数据验证
-        if(findUserByName!=null){
-            if(user.getUserName().equals(findUserByName.getUserName())){
-                verify.put("nameError", "用户名已被注册");
-            }
-            if(user.getEmail().equals(findUserByEmail.getEmail())){
-                verify.put("emailError", "邮箱已被注册");
-            }
-            if(user.getMobile().equals(findUserByMobile.getMobile())){
-                verify.put("mobileError", "该手机已被注册");
-            }
-//            mv.setViewName(UserConstant.REGISTRY);
-            return JSON.toJSONString(verify);
-        }
-        return null;
-    }
+
+
 
 
 }
